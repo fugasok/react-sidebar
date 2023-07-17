aa60e3c691089c864767ae903c5febfbadc7a031
Tibor <tibor736@hotmail.com>
2023-07-18 02:44:29 +0300

    02:modal window css and logic added

--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,6 +1,20 @@
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+
 const Modal = () => {
-  return (
-    <div>Modal</div>
-  )
-}
-export default Modal
+	const { closeModal, isModalOpen } = useGlobalContext();
+
+	return (
+		<div
+			className={isModalOpen ? "modal-overlay show-modal" : "modal-overlay"}
+		>
+			<div className="modal-container">
+				<h3>modal content</h3>
+				<button className="close-modal-btn" onClick={closeModal}>
+					<FaTimes />
+				</button>
+			</div>
+		</div>
+	);
+};
+export default Modal;
